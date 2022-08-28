5b236fd9e90d0ea75b1f979cae992f7627224055
Maciej Sroka <srokatonie@gmail.com>
2022-08-28 12:36:35 +0200

    Fails in HashConnect init

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,66 +1,26 @@
 <script lang="ts">
-	import Counter from '$lib/Counter.svelte';
 	
-	import { onMount } from 'svelte'
+import { onMount } from 'svelte'
 
-	let hashConnect
-	onMount(async () => {
-		const { HashConnect } = await import('hashconnect')
-		hashConnect = new HashConnect()
-	})
+let hashconnect
+onMount(async () => {
+	const { HashConnect } = await import('hashconnect')
+
+	hashconnect = new HashConnect(true)
+
+	let appMetadata = {
+		name: "dApp Example",
+		description: "An example hedera dApp",
+		icon: "/svelte-welcome.png"
+	}
+
+	// This fails:
+	let initData = await hashconnect.init(appMetadata, "testnet");
+
+})
 
 </script>
 
-<svelte:head>
-	<title>Home</title>
-	<meta name="description" content="Svelte demo app" />
-</svelte:head>
-
-<section>
-	<h1>
-		<span class="welcome">
-			<picture>
-				<source srcset="svelte-welcome.webp" type="image/webp" />
-				<img src="svelte-welcome.png" alt="Welcome" />
-			</picture>
-		</span>
-
-		to your new<br />SvelteKit app
-	</h1>
-
-	<h2>
-		try editing <strong>src/routes/+page.svelte</strong>
-	</h2>
-
-	<Counter />
-</section>
-
-<style>
-	section {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		flex: 1;
-	}
-
-	h1 {
-		width: 100%;
-	}
-
-	.welcome {
-		display: block;
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
-	}
-
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
-	}
-</style>
+<h1>
+	Testing HashConnect
+</h1>
