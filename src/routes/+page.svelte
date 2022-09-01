7eaa800c8281e90e24885f832d074da4b24056c6
Maciej Sroka <srokatonie@gmail.com>
2022-09-01 08:29:37 +0200

    Updated package name, small edits

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,24 +1,55 @@
 <script lang="ts">
+  import { onMount } from "svelte";
+  import { Buffer } from "buffer";
+
+  let hashconnect: any
+	let pairingString: string
+	let connectionMessage: string | null
+	let connected = false
+	let tokenSymbol = 'TTT1';
+
+  onMount(async () => {
+    global.Buffer = Buffer;
+
+    const { HashConnect } = await import("hashconnect");
+
+    hashconnect = new HashConnect()
+
+    let appMetadata = {
+      name: "dApp Example",
+      description: "An example hedera dApp",
+      icon: "/svelte-welcome.png",
+    }
+
+		registerEvents()
+
+    let initData = await hashconnect.init(appMetadata, "testnet")
+		console.log({initData})
+		const state = await hashconnect.connect()
+		console.log({state})
+		pairingString = hashconnect.generatePairingString(state, "testnet", false)
+		hashconnect.findLocalWallets()
+		if (pairingString) {
+			connected = true
+		}
+  })
+
+	function registerEvents() {
+		hashconnect.foundExtensionEvent.once((walletMetadata: any) => {
+			console.log({walletMetadata})
+			hashconnect.connectToLocalWallet(pairingString)
+		})
 	
-import { onMount } from 'svelte'
-
-let hashconnect
-onMount(async () => {
-	const { HashConnect } = await import('hashconnect')
-
-	hashconnect = new HashConnect(true)
-
-	let appMetadata = {
-		name: "dApp Example",
-		description: "An example hedera dApp",
-		icon: "/svelte-welcome.png"
+		hashconnect.pairingEvent.once((pairingData: any) => {
+			console.log({pairingData})
+			connected = true
+			connectionMessage = null
+		})
+	
+		hashconnect.acknowledgeMessageEvent.once((acknowledgeData: any) => {
+			console.log({acknowledgeData})
+		})
 	}
-
-	// This fails:
-	let initData = await hashconnect.init(appMetadata, "testnet");
-
-})
-
 </script>
 
 <h1>
--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,7 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import type { UserConfig } from 'vite';
 
 const config: UserConfig = {
-	plugins: [sveltekit()],
-	define: {
-    global: {},
-  },
+	plugins: [sveltekit()]
 };
 
 export default config;
